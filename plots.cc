b5939154ccd622111908c7de5f9f70ec171c71e5
Zepyoor Khechadoorian <zkhechad@gm2cornellsrv1.fnal.gov>
2024-02-02 09:58:51 -0600

    separately saving A_y1 plots

--- plots.cc
+++ plots.cc
@@ -365,7 +365,8 @@ int main(int argc, char* argv[]){
         gay1[i].SetLineColor(1);
         gay1[i].SetMarkerColor(1);
         gay1[i].SetTitle(Form("calo%i A_y1", i+1));
-        gay1[i].Draw("A_y1.pdf");
+        gay1[i].Draw("APE");
+        c.Print("A_y1.pdf");
         gay1[i].SetLineColor(i+1);
         gay1[i].SetMarkerColor(i+1);
         mgay1.Add(&gay1[i]);
