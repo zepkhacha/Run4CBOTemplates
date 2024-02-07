b478cf2524f05faa477d08b340bda41f2307b4f5
Zepyoor Khechadoorian <zkhechad@gm2cornellsrv1.fnal.gov>
2024-02-07 13:39:38 -0600

    debugging template maker; restoring old PDFs of alpha v time

--- plotAll.cc
+++ plotAll.cc
@@ -1,23 +1,23 @@
 void plotAll(){
 
-    //plotParam("slidingFits/noRF_windowFits.root", "alpha_CBO", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
-    //plotParam("slidingFits/noRF_windowFits.root", "beta_CBO", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
-    //plotParam("slidingFits/noRF_windowFits.root", "A_CBO", "dimensionless", 0.0, 0.012, "ALE PFC PLC");
-    //plotParam("slidingFits/noRF_windowFits.root", "phi_CBO", "[rad]", -3.2, 3.2, "ALE PFC PLC");
+    plotParam("slidingFits/noRF_windowFits.root", "alpha_CBO", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
+    plotParam("slidingFits/noRF_windowFits.root", "beta_CBO", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
+    plotParam("slidingFits/noRF_windowFits.root", "A_CBO", "dimensionless", 0.0, 0.012, "ALE PFC PLC");
+    plotParam("slidingFits/noRF_windowFits.root", "phi_CBO", "[rad]", -3.2, 3.2, "ALE PFC PLC");
 
-    //plotParam("slidingFits/noRF_windowFits.root", "alpha_2CBO", "dimensionless", -0.002, 0.002, "ALE PFC PLC");
-    //plotParam("slidingFits/noRF_windowFits.root", "beta_2CBO", "dimensionless", -0.002, 0.002, "ALE PFC PLC");
-    //plotParam("slidingFits/noRF_windowFits.root", "A_2CBO", "dimensionless", 0.0, 0.002, "ALE PFC PLC");
+    plotParam("slidingFits/noRF_windowFits.root", "alpha_2CBO", "dimensionless", -0.002, 0.002, "ALE PFC PLC");
+    plotParam("slidingFits/noRF_windowFits.root", "beta_2CBO", "dimensionless", -0.002, 0.002, "ALE PFC PLC");
+    plotParam("slidingFits/noRF_windowFits.root", "A_2CBO", "dimensionless", 0.0, 0.002, "ALE PFC PLC");
     plotParam("slidingFits/noRF_windowFits.root", "phi_2CBO", "[rad]", -3.2, 3.2, "ALE PFC PLC");
 
-    //plotParam("slidingFits/noRF_windowFits.root", "alpha_y", "dimensionless", -0.002, 0.002, "ALE PFC PLC");
-    //plotParam("slidingFits/noRF_windowFits.root", "beta_y", "dimensionless", -0.002, 0.002, "ALE PFC PLC");
-    //plotParam("slidingFits/noRF_windowFits.root", "A_y", "dimensionless", 0.0, 0.002, "ALE PFC PLC");
-    //plotParam("slidingFits/noRF_windowFits.root", "phi_y", "[rad]", -3.2, 3.2, "ALE PFC PLC");
+    plotParam("slidingFits/noRF_windowFits.root", "alpha_y", "dimensionless", -0.002, 0.002, "ALE PFC PLC");
+    plotParam("slidingFits/noRF_windowFits.root", "beta_y", "dimensionless", -0.002, 0.002, "ALE PFC PLC");
+    plotParam("slidingFits/noRF_windowFits.root", "A_y", "dimensionless", 0.0, 0.002, "ALE PFC PLC");
+    plotParam("slidingFits/noRF_windowFits.root", "phi_y", "[rad]", -3.2, 3.2, "ALE PFC PLC");
 
-    //plotParam("slidingFits/noRF_windowFits.root", "alpha_vw", "dimensionless", -0.002, 0.002, "ALE PFC PLC");
-    //plotParam("slidingFits/noRF_windowFits.root", "beta_vw", "dimensionless", -0.002, 0.002, "ALE PFC PLC");
-    //plotParam("slidingFits/noRF_windowFits.root", "A_vw", "dimensionless", 0.0, 0.002, "ALE PFC PLC");
-    //plotParam("slidingFits/noRF_windowFits.root", "phi_vw", "[rad]", -3.2, 3.2, "ALE PFC PLC");
+    plotParam("slidingFits/noRF_windowFits.root", "alpha_vw", "dimensionless", -0.002, 0.002, "ALE PFC PLC");
+    plotParam("slidingFits/noRF_windowFits.root", "beta_vw", "dimensionless", -0.002, 0.002, "ALE PFC PLC");
+    plotParam("slidingFits/noRF_windowFits.root", "A_vw", "dimensionless", 0.0, 0.002, "ALE PFC PLC");
+    plotParam("slidingFits/noRF_windowFits.root", "phi_vw", "[rad]", -3.2, 3.2, "ALE PFC PLC");
 
 }
