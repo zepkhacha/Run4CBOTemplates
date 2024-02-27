e0cbde83266f4114c82a75ad90b90b86cd2b7fc4
Zepyoor Khechadoorian <zkhechad@gm2cornellsrv1.fnal.gov>
2024-02-27 12:48:38 -0600

    made templates1 from slidingFits1

--- makeAll.cc
+++ makeAll.cc
@@ -4,16 +4,16 @@ void makeAll(){
 //    makeTemplate("slidingFits/noRF_windowFits.root", "/gm2data/zkhechad/cboTemplates/Run4CBOTemplates/fullFits/noRF/sBin_constraintOn_cE0.0_seed0_noRF_calo", "beta_CBO", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
 //
 
-    makeTemplate("slidingFits0/noRF_windowFits.root", "/gm2data/zkhechad/cboTemplates/Run4CBOTemplates/fullFits/noRF/sBin_constraintOn_cE0.0_seed0_noRF_calo", "alpha_CBO", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
-    makeTemplate("slidingFits0/noRF_windowFits.root", "/gm2data/zkhechad/cboTemplates/Run4CBOTemplates/fullFits/noRF/sBin_constraintOn_cE0.0_seed0_noRF_calo", "beta_CBO", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
+    makeTemplate("slidingFits1/noRF_windowFits.root", "/gm2data/zkhechad/cboTemplates/Run4CBOTemplates/fullFits/noRF/sBin_constraintOn_cE0.0_seed0_noRF_calo", "alpha_CBO", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
+    makeTemplate("slidingFits1/noRF_windowFits.root", "/gm2data/zkhechad/cboTemplates/Run4CBOTemplates/fullFits/noRF/sBin_constraintOn_cE0.0_seed0_noRF_calo", "beta_CBO", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
 
-    makeTemplate("slidingFits0/noRF_windowFits.root", "/gm2data/zkhechad/cboTemplates/Run4CBOTemplates/fullFits/noRF/sBin_constraintOn_cE0.0_seed0_noRF_calo", "alpha_2CBO", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
-    makeTemplate("slidingFits0/noRF_windowFits.root", "/gm2data/zkhechad/cboTemplates/Run4CBOTemplates/fullFits/noRF/sBin_constraintOn_cE0.0_seed0_noRF_calo", "beta_2CBO", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
+    makeTemplate("slidingFits1/noRF_windowFits.root", "/gm2data/zkhechad/cboTemplates/Run4CBOTemplates/fullFits/noRF/sBin_constraintOn_cE0.0_seed0_noRF_calo", "alpha_2CBO", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
+    makeTemplate("slidingFits1/noRF_windowFits.root", "/gm2data/zkhechad/cboTemplates/Run4CBOTemplates/fullFits/noRF/sBin_constraintOn_cE0.0_seed0_noRF_calo", "beta_2CBO", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
 
-    makeTemplate("slidingFits0/noRF_windowFits.root", "/gm2data/zkhechad/cboTemplates/Run4CBOTemplates/fullFits/noRF/sBin_constraintOn_cE0.0_seed0_noRF_calo", "alpha_y", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
-    makeTemplate("slidingFits0/noRF_windowFits.root", "/gm2data/zkhechad/cboTemplates/Run4CBOTemplates/fullFits/noRF/sBin_constraintOn_cE0.0_seed0_noRF_calo", "beta_y", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
+    makeTemplate("slidingFits1/noRF_windowFits.root", "/gm2data/zkhechad/cboTemplates/Run4CBOTemplates/fullFits/noRF/sBin_constraintOn_cE0.0_seed0_noRF_calo", "alpha_y", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
+    makeTemplate("slidingFits1/noRF_windowFits.root", "/gm2data/zkhechad/cboTemplates/Run4CBOTemplates/fullFits/noRF/sBin_constraintOn_cE0.0_seed0_noRF_calo", "beta_y", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
 
-    makeTemplate("slidingFits0/noRF_windowFits.root", "/gm2data/zkhechad/cboTemplates/Run4CBOTemplates/fullFits/noRF/sBin_constraintOn_cE0.0_seed0_noRF_calo", "alpha_vw", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
-    makeTemplate("slidingFits0/noRF_windowFits.root", "/gm2data/zkhechad/cboTemplates/Run4CBOTemplates/fullFits/noRF/sBin_constraintOn_cE0.0_seed0_noRF_calo", "beta_vw", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
+    makeTemplate("slidingFits1/noRF_windowFits.root", "/gm2data/zkhechad/cboTemplates/Run4CBOTemplates/fullFits/noRF/sBin_constraintOn_cE0.0_seed0_noRF_calo", "alpha_vw", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
+    makeTemplate("slidingFits1/noRF_windowFits.root", "/gm2data/zkhechad/cboTemplates/Run4CBOTemplates/fullFits/noRF/sBin_constraintOn_cE0.0_seed0_noRF_calo", "beta_vw", "dimensionless", -0.01, 0.01, "ALE PFC PLC");
 
 }
